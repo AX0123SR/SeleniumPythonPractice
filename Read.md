c762dba2d922d534d826025f1f28c3d9747b2856
Ayush Srivastava <comeayush2@gmail.com>
2024-05-19 16:03:46 +0530

    New file

--- Read.md
+++ Read.md
@@ -8,3 +8,19 @@ Parameterization
      It is a technique to test the same function with different set 
      of values.
 
+Implicit Wait
+
+    It tells the Webdriver the maximum wait time for searching for any
+    elements before throwing any exception.
+    Implicit wait is global it is applicable for all the WebElements.
+
+Explicit Wait
+
+    It tells the Webdriver to halt the execution until a specific condition
+    is met or the maximum time has passed.
+    Explicit wait is applicable to webelements which are specified by user.
+
+Fluent Wait
+
+    It is a way to specify the maximum time to wait for a specific comdition met
+    as well as frequency with which to check the condition.
--- tests/test_autoSuggestion.py
+++ tests/test_autoSuggestion.py
@@ -14,14 +14,12 @@ def test_autoSuggestion():
     driver.maximize_window()
     depart_city = driver.find_element(By.XPATH,"//input[@id='BE_flight_origin_city']")
     depart_city.click()
-    time.sleep(2)
     depart_city.send_keys("New Delhi")
-    time.sleep(2)
+    # time.sleep(1)
     depart_city.send_keys(Keys.ENTER)
     time.sleep(2)
     arrival_city = driver.find_element(By.XPATH,"//input[@id='BE_flight_arrival_city']")
     arrival_city.click()
-    time.sleep(2)
     arrival_city.send_keys("New")
     time.sleep(2)
     search_list = driver.find_elements(By.XPATH,"//div[@class='viewport']/div/div/li")
@@ -29,17 +27,16 @@ def test_autoSuggestion():
     for search in search_list:
         print(search.text)
         if "New Orleans (MSY)" in search.text:
-            time.sleep(2)
             search.click()
+            time.sleep(2)
             break
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_date']"))).click()
 
-    driver.find_element(By.XPATH,"//input[@id='BE_flight_origin_date']").click()
-    time.sleep(4)
-    all_dates = driver.find_elements(By.XPATH,"//table[@class='days-head day-container-table']/tbody/tr/td[@class!='inActiveTD']")
+    all_dates = (wait.until((EC.element_to_be_clickable(By.XPATH,"//table[@class='days-head day-container-table']/tbody/tr/td[@class!='inActiveTD']"))).find_elements(By.XPATH,"//table[@class='days-head day-container-table']/tbody/tr/td[@class!='inActiveTD']"))
     for date in all_dates:
         if date.get_attribute("data-date") == "12/01/2025":
             date.click()
-            time.sleep(4)
             break
 
     driver.find_element(By.XPATH,"//input[@value='Search Flights']").click()
